5f8a08afa722a4fca197bb1065550cb7fe684a2c
Rafael Andrade <rafaelandrade@wizsolucoes.com.br>
2019-08-23 16:47:02 -0300

    Remover projeto lib do analytics

--- projects/ng-google-analytics/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ng-google-analytics
- */
-
-export * from './lib/ng-google-analytics.service';
-export * from './lib/ng-google-analytics.component';
-export * from './lib/ng-google-analytics.module';
